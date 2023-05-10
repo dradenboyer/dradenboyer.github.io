a1c1037ce33bee38b125e7d133a0a842d1ca50c5
Draden Boyer <dradenboyer@gmail.com>
2023-05-10 00:55:57 -0600

    this isnt working

--- index.js
+++
@@ -1,6 +0,0 @@
-import initScrollReveal from "./simplefolio/src/scripts/scrollReveal";
-import initTiltEffect from "./simplefolio/src/scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./simplefolio/src/data/scrollRevealConfig";
-
-initScrollReveal(targetElements, defaultProps);
-initTiltEffect();
